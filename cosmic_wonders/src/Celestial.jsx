a591cbad50ae05bdc76021dd33b0e00f202c1621
ravisaxena23 <saxenaravi23599@gmail.com>
2020-08-09 09:54:03 +0530

    solved error by incrementing key value

--- cosmic_wonders/src/Celestial.jsx
+++ cosmic_wonders/src/Celestial.jsx
@@ -14,8 +14,7 @@ class Celestial extends Component{
 
 onClick(id){
     this.setState({clicked:id})
-}
-
+};
 showDetails(){
   if(this.state.clicked!==-1)
   return(
@@ -27,15 +26,18 @@ showDetails(){
           {this.state.wonders[this.state.clicked].description}
         </Col>
       </Row>
-
     </div>
   )
 }
 
-beautify(wonder){
-  const beautiful=wonder.map(wonders=>{
+beautify(wonders){
+  const beautiful=wonders.map((wonders,i)=>{
+    i=i+1;
     return(
-      <div key={this.state.wonders.id} className="col-md-3">{wonders}</div>
+      <div key={wonders.key+i} id={wonders.id} className="col-md-3">{wonders}
+      {console.log(wonders.key)}
+      {/* problem is here */}
+      </div>
       )
   })
   return(beautiful)
@@ -43,7 +45,10 @@ beautify(wonder){
   render(){
       const displayWonder=this.state.wonders.map(wonders=>{
         return(
-          <LoneWonder key={wonders.id} info={wonders} onClickBtn={(id)=>this.onClick(id)} />
+          <div>
+          <LoneWonder key={wonders.id} id={wonders.id} info={wonders} onClickBtn={(id)=>this.onClick(id)} />
+          {/*working console.log(wonders.id)*/}
+          </div>
     )})
       return(
     <div key={this.state.wonders.id} className="container">
