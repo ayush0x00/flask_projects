6d11eb9581de544b35a5b573fce1eb938c40f6f6
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-08-28 00:36:38 +0530

    Uncontrolled from

--- cosmic_wonders/src/Header.jsx
+++ cosmic_wonders/src/Header.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand, Nav , NavItem, Jumbotron} from 'reactstrap';
+import {Navbar, NavbarBrand, Nav , NavItem, Jumbotron,Modal, ModalBody, ModalHeader,
+Form, FormGroup, Input, Label,Button} from 'reactstrap';
 import {NavLink} from 'react-router-dom'
 class Header extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      isModalOpen:false
+    }
+    this.toggleModal=this.toggleModal.bind(this);
+    this.handleLogin=this.handleLogin.bind(this)
+  }
+
+  toggleModal(){
+    this.setState({isModalOpen:!this.state.isModalOpen})
+  }
+
+  handleLogin(event){
+    this.toggleModal();
+    alert(this.username.value)
+    event.preventDefault()
+  }
+
   render(){
     return(
       <>
@@ -22,6 +42,11 @@ class Header extends Component{
                 <NavLink className="nav-link" style={{color:"black"}} to="/feedback">Feedback</NavLink>
               </NavItem>
             </Nav>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Button outline onClick={this.toggleModal}> SignIn </Button>
+              </NavItem>
+            </Nav>
           </div>
         </Navbar>
         <Jumbotron>
@@ -34,6 +59,27 @@ class Header extends Component{
             </div>
             </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} >
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+          <Form onSubmit={this.handleLogin}>
+          <FormGroup>
+          <Label htmlFor="username">Username</Label>
+          <Input type="text" id="username" name="username" innerRef={(input)=>this.username=input}/>
+          <Label htmlFor="password">password</Label>
+          <Input type="password" id="password" name="password" innerRef={input=>this.password=input}/>
+          </FormGroup>
+          <FormGroup check>
+          <Input type="checkbox" name="remember" innerRef={input=>this.remember=input}/>
+            <Label check htmlFor="remember"> Remember me
+            </Label>
+          </FormGroup>
+          <FormGroup>
+            <Button type="submit" value="submit" color="primary" onClick={this.handleLogin}>Login </Button>
+            </FormGroup>
+            </Form>
+          </ModalBody>
+          </Modal>
       </>
     )
   }
