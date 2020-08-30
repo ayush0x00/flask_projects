c44b534dafb21a52b3ba12727edb0a4321fe67d9
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-08-31 01:18:19 +0530

    Added Comments

--- cosmic_wonders/src/CelestialDetail.jsx
+++ cosmic_wonders/src/CelestialDetail.jsx
@@ -1,17 +1,27 @@
 import React,{Component} from 'react';
-import {Button, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Button, Breadcrumb, BreadcrumbItem,Label,Row,Col,Modal,ModalBody
+,ModalHeader,ModalFooter} from 'reactstrap';
+import {Control,LocalForm} from 'react-redux-form'
 import {Link} from 'react-router-dom';
 
 class CelestialDetail extends Component{
   constructor (props){
     super(props);
+    this.state={
+      isModalOpen:false
+    }
     this.handleClick=this.handleClick.bind(this);
+    this.handleLogin=this.handleLogin.bind(this);
   }
 handleClick=()=>{
-  const component=()=>{
-    return(<div>Hello</div>)}
-  return component
+  this.setState({
+    isModalOpen:!this.state.isModalOpen
+  })
 }
+handleLogin=(values)=>{
+  alert(JSON.stringify(values))
+}
+
   render(){
     return(
       <div className="container">
@@ -26,6 +36,30 @@ handleClick=()=>{
           </div>
           <div className="col"> <h3>{this.props.id.name} </h3>
           <Button className="primary" onClick={this.handleClick}>Add a comment </Button>
+          <Modal isOpen={this.state.isModalOpen} toggle={this.handleClick}>
+            <ModalHeader toggle={this.handleClick}>Comment </ModalHeader>
+            <ModalBody>
+              <LocalForm onSubmit={(values)=>this.handleLogin(values)}>
+                <Row className="form-group">
+                <Col >
+                  <Label htmlFor="username">Your name </Label>
+                  <Control.text model=".username" name="username"
+                  className="form-control " />
+                </Col>
+                </Row>
+                <Row className="form-group">
+                <Col>
+                <Label  htmlFor="feedback">Feedback </Label>
+                <Control.textarea model=".feedback" name="feedback" className="form-control" />
+                </Col>
+                </Row>
+                <ModalFooter>
+                <Button type="submit" onClick={this.handleClick}color="primary">Comment </Button>
+                </ModalFooter>
+              </LocalForm>
+              </ModalBody>
+              </Modal>
+
           </div>
           <div className="col"> {this.props.id.description}  </div>
         </div>
